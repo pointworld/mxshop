eee827107fd858ad2fda12a6632f4ea21d4653fd
one <onerf@sina.com>
2018-12-19 12:44:44 +0800

    init project env and structure

---
+++ extra_apps/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# _*_ coding: utf8 _*_
+
+__author__ = 'point'
+__date__ = '2018-12-19'
--- mxshop/settings.py
+++ mxshop/settings.py
@@ -11,10 +11,15 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+sys.path.insert(0, BASE_DIR)
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+sys.path.insert(0, os.path.join(BASE_DIR, 'extra_apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -76,8 +81,15 @@ WSGI_APPLICATION = 'mxshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'mxshop',
+        'USER': 'root',
+        'PASSWORD': '1.',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'OPTIONS': { 'init_command': 'SET default_storage_engine=INNODB;' },
     }
 }
 
