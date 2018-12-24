a1abcf6f97797d439fb023b64aa51cdd0cdb4c9e
one <onerf@sina.com>
2018-12-25 00:09:18 +0800

    feat: user register

--- apps/users/views.py
+++ apps/users/views.py
@@ -8,6 +8,8 @@ from rest_framework.mixins import CreateModelMixin
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 
+from rest_framework_jwt.serializers import jwt_payload_handler, jwt_encode_handler
+
 from mxshop.settings import APIKEY
 from users.models import AuthCode
 from .serializers import SmsSerializer, UserRegisterSerializer
@@ -84,4 +86,21 @@ class UserViewSet(CreateModelMixin, viewsets.GenericViewSet):
     """
     serializer_class = UserRegisterSerializer
     queryset = User.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = self.perform_create(serializer)
+
+        ret_dict = serializer.data
+        payload = jwt_payload_handler(user)
+        ret_dict['token'] = jwt_encode_handler(payload)
+        ret_dict['name'] = user.name if user.name else user.username
+
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        return serializer.save()
+
 
